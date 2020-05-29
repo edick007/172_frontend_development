74f9130c6babee39d0e8f2de86980df2a30768a1
Edward Dickhoff <32476243+edick007@users.noreply.github.com>
2020-05-28 19:44:32 -0700

    react-bootsrap set up

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const About = () => (
+<div>
+    <h2>About Page</h2>
+    <p> PARA ! tlajsdnfkjasdhfioawne;ilf3awnio;'fwaio</p>
+    <p> PARA ! tlajsdnfkjasdhfioawne;ilf3awnio;'fwaio</p>
+    <p> PARA ! tlajsdnfkjasdhfioawne;ilf3awnio;'fwaio</p>
+    <p> PARA ! tlajsdnfkjasdhfioawne;ilf3awnio;'fwaio</p>
+</div>
+
+)
---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Home = () => (
+<div>
+    <h2>HI</h2>
+    <p> PARA ! tlajsdnfkjasdhfioawne;ilf3awnio;'fwaio</p>
+    <p> PARA ! tlajsdnfkjasdhfioawne;ilf3awnio;'fwaio</p>
+    <p> PARA ! tlajsdnfkjasdhfioawne;ilf3awnio;'fwaio</p>
+    <p> PARA ! tlajsdnfkjasdhfioawne;ilf3awnio;'fwaio</p>
+</div>
+
+)
